2eafe87a684840c9df6d98fae49572fa48f7d979
sw3ak <v.bankovskiy03@mail.ru>
2024-11-23 14:26:04 +0700

    Forth lab

---
+++ 1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Вывод структуры и содержимого таблицы</title>
+    <?php
+    include("3.inc"); // Задание стилей
+    include("4.inc"); // Подключение к базе данных
+    include("5.inc"); // Вывод структуры/содержимого таблицы
+    include("6.inc"); // Отключение от базы данных
+    ?>
+</head>
+<body>
+</body>
+</html>
